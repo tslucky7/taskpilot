874f94f262db84f62833ede0d839dbf8a382ed79
齊藤拓海 <takumi1222122212221222@gmail.com>
2026-01-28 09:00:40 +0900

    feat: add,remove,clear,all methods to taskStore

--- web/src/store/taskStore.ts
+++ web/src/store/taskStore.ts
@@ -1,24 +1,45 @@
-import type { Task } from "../model/task";
+import { createTask, type Task } from '../model/task';
 
 export class TaskStore {
   private tasks: Task[] = [];
 
-  public add(title: Task["title"]) {
-    title = title.trim();
-    if (title === "") {
-      throw new Error("タスク名を入力してください");
-    };
+  /**
+   * タスクを追加する
+   *
+   * @param title タスク名
+   */
+  public add(title: string): void {
+    const trimmedTitle = title.trim();
+    if (trimmedTitle === '') {
+      throw new Error('タスク名を入力してください');
+    }
+
+    this.tasks = [...this.tasks, createTask(trimmedTitle)]; // 非破壊的に変更。
   }
 
-  public remove(id: Task["id"]) {
+  /**
+   * タスクを削除する
+   *
+   * @param id タスクID
+   * @returns 削除成功かどうか
+   */
+  public remove(id: number): boolean {
+    const before = this.tasks.length;
+    this.tasks = this.tasks.filter((t) => t.id !== id);
 
+    return this.tasks.length !== before;
   }
 
-  public clear() {
-
+  public clear(): void {
+    this.tasks = []; // 非破壊的に変更。
   }
 
-  public all() {
-
+  /**
+   * すべてのタスクを取得する
+   *
+   * @returns タスク配列
+   */
+  public all(): Task[] {
+    return [...this.tasks]; // 非破壊的に変更。
   }
 }
