24bf702f7067acc7df7b62cf4d078e0a62430ca9
齊藤拓海 <takumi1222122212221222@gmail.com>
2026-01-30 09:27:27 +0900

    feat: TaskAppの各アクション実装
    - onSubmit: タスク追加
    - onClear: タスク全削除
    - render: タスク一覧表示
    - createCell: セル作成
    - createDeleteBtn: 削除ボタン作成

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
-import { TaskStore } from './store/taskStore'
-import { TaskApp } from './ui/TaskApp'
+import './style.css';
+import { TaskStore } from './store/taskStore';
+import { TaskApp } from './ui/TaskApp';
 
-const store = new TaskStore()
-const app = new TaskApp(store)
-app.mount()
+const store = new TaskStore();
+const app = new TaskApp(store);
+app.mount();
 
--- web/src/ui/TaskApp.ts
+++ web/src/ui/TaskApp.ts
@@ -1,4 +1,5 @@
 import type { TaskStore } from "../store/taskStore";
+import type { Task } from "../model/task";
 
 export class TaskApp {
   private readonly taskStore: TaskStore;
@@ -29,19 +30,70 @@ export class TaskApp {
   };
 
   mount(): void {
-    this.form.addEventListener('submit', this.onSubmit);
-    this.clearBtn.addEventListener('click', this.onClear);
+    this.form.addEventListener('submit', this.onSubmit.bind(this));
+    this.clearBtn.addEventListener('click', this.onClear.bind(this));
     this.render();
   };
 
   onSubmit(event: Event): void {
-    debugger;
-    console.log('onSubmit this=', this);
+    event.preventDefault();
+
+    try {
+      const title = this.input.value.trim();
+      if (title === '') {
+        return;
+      }
+
+      this.taskStore.add(title);
+      this.input.value = '';
+
+      this.render();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   onClear(): void {
-
+    this.taskStore.clear();
+    this.render();
   };
 
-  render(): void {};
+  render(): void {
+    const tasks = this.taskStore.all();
+    this.count.textContent = tasks.length.toString();
+    // this.count.textContent = String(tasks.length);
+
+    this.rows.textContent = '';
+    tasks.forEach((task) => {
+      const row = document.createElement('tr');
+
+      const idCell = this.createCell(task.id.toString());
+      row.appendChild(idCell);
+
+      const titleCell = this.createCell(task.title);
+      row.appendChild(titleCell);
+
+      const deleteBtn = this.createDeleteBtn(task);
+      row.appendChild(deleteBtn);
+
+      this.rows.appendChild(row);
+    });
+  };
+
+  private createCell(content: string): HTMLTableCellElement {
+    const cell = document.createElement('td');
+    cell.textContent = content;
+    return cell;
+  }
+
+  private createDeleteBtn(task: Task): HTMLButtonElement {
+    const button = document.createElement('button');
+    button.textContent = '削除';
+
+    button.addEventListener('click', () => {
+      this.taskStore.remove(task.id);
+      this.render();
+    });
+    return button;
+  }
 }
